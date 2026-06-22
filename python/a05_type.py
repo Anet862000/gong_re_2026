989b5efc271afcf7658379abb652fb51cd04e3ef
Anet862000 <younwoo1744@gmail.com>
2026-06-22 15:44:54 +0900

    type

---
+++ python/a05_type.py
@@ -0,0 +1,14 @@
+def main():
+    int_a = 123
+    print(int_a, type(int_a))
+    int_b = "123 숫자"
+    print(int_b, type(int_b))
+    float_a = 3.14
+    print(float_a, type(float_a))
+    str_a = "Hello, World!"
+    print(str_a, type(str_a))
+    bool_a = True
+    print(bool_a, type(bool_a))
+
+if __name__ == "__main__":
+    main()
