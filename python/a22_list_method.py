ba634bbbe541035fd1ac12de1e70c730160be5b3
Anet862000 <younwoo1744@gmail.com>
2026-06-23 14:19:34 +0900

    list method

--- python/a22_list_method.py
+++ python/a22_list_method.py
@@ -1,3 +1,14 @@
+class Mylist:
+    def __init__(self):
+        self.myVariable = "Lee" #인스턴스 변수
+        self.myVariable2 = "Youn"  #인스턴스 변수
+        self.mylist = list()
+
+    def append(self, element):
+        self.mylist.append(element) #메소드
+        
+
+
 def main():
     list_a = [1, 2, 3]
     list_b = [4, 5, 6]
@@ -13,6 +24,10 @@ def main():
 
     list_b.insert(1, 4.5) #리스트에 요소 삽입하기
     print(list_b)
+
+    myList_a = Mylist()
+    myList_a.append("Lee Younwoo")
+    print(myList_a.myVariable, myList_a.myVariable2, myList_a.mylist)
 
 if __name__ == '__main__':
     main()
