2d206d96b76b39d84ee78c45c0d8b408d821d08b
Anet862000 <younwoo1744@gmail.com>
2026-06-23 10:29:37 +0900

    if elfi if elif

--- python/a17_datetime2.py
+++ python/a17_datetime2.py
@@ -11,15 +11,27 @@ def main():
         print(f"현재 시간은 {now.hour}시로 오후입니다.")
 
 
-    print(now.month, type(now.month))
+    now_month = int(input("현재 월을 입력하세요: "))
+    print(now_month, type(now_month))
 
-    if now.month == 12 or 1 <= now.month <= 3:
+    if now_month == 12 or 1 <= now_month <= 3:
         print("겨울")
-    elif 4 <= now.month <= 5:
+    elif 4 <= now_month <= 5:
         print("봄")
-    elif 6 <= now.month <= 8:
+    elif 6 <= now_month <= 8:
         print("여름")
+    elif 9 <= now_month <= 11:
+        print("가을")
     else:
+        print("해당 월은 존재하지 않습니다.")
+
+    if now.month in [12, 1, 2, 3]:
+        print("겨울")
+    elif now.month in [4, 5]:
+        print("봄")
+    elif now.month in [6, 7, 8]:
+        print("여름")
+    elif now.month in [9, 10, 11]:
         print("가을")
 
 if __name__ == "__main__":
