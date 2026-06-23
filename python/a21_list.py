e6af7efcfcf6cb9dd28d945c7bf53a0a1914f69c
Anet862000 <younwoo1744@gmail.com>
2026-06-23 13:57:29 +0900

    list

--- python/a21_list.py
+++ python/a21_list.py
@@ -14,3 +14,15 @@ def main():
     print(list_c[-1])
     list_c[0] = 'youn'
     print(list_c)
+
+    list_d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] #0 ~ n-1까지
+    print(list_d[1][2]) #6
+    print(list_d[0][1]) #2
+
+    print(len(list_d)) #개수 확인
+    print(list_d.__len__()) #개수 확인
+
+
+
+if __name__ == '__main__':
+    main()
