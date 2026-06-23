95bbf164d93ef1cecf60fbafc57163711b6b7e8b
Anet862000 <younwoo1744@gmail.com>
2026-06-23 11:45:32 +0900

    odd

--- python/a20_odd_elif.py
+++ python/a20_odd_elif.py
@@ -1,10 +1,12 @@
 def main():
     number = int(input("정수를 입력하세요: "))
 
-    if number % 2 == 0:
-        print(f"{number}는 짝수입니다.")
-    else:
+    if number % 2:  # number % 2가 0이 아니면 참이므로 홀수입니다.(0 1 -> 0이면 false)
         print(f"{number}는 홀수입니다.")
+    else:
+        print(f"{number}는 짝수입니다.")
+    
+    print("홀수" if number % 2 else "짝수", "\b입니다.")  # 삼항 연산자
 
 if __name__ == "__main__":
     main()
