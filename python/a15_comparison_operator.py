53aacfbc77c932d51eabf54e4e4ced9696ece823
Anet862000 <younwoo1744@gmail.com>
2026-06-23 09:55:46 +0900

    new lines

--- python/a15_comparison_operator.py
+++ python/a15_comparison_operator.py
@@ -11,5 +11,11 @@ def main():
     print(True and False)
     print(False and False)
 
+    a = int(input("100보다 큰 수를 입력하세요: "))
+
+    if a > 100:
+        print("100보다 큰 수를 입력하셨습니다.")
+    print("프로그램이 종료되었습니다.")
+
 if __name__ == "__main__":
     main()
